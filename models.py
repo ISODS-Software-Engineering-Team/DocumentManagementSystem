009b854e8938b8dee9fc1bef449a925f3af74c00
tientruongisods <127987939+tientruongisods@users.noreply.github.com>
2023-03-24 16:40:28 -0700

    Document

---
+++ models.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+class Category(models.Model):
+    category_id = models.CharField(max_length=20)
+    category_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.category_id
+
+    class Meta:
+        db_table = 'Category'
+
+
+class Document(models.Model):
+    docs_id = models.CharField(max_length=50)
+    category_id = models.CharField(max_length=20)
+    brief = models.CharField(max_length=50)
+    content = models.CharField(max_length=500)
+    media_file = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    created_date = models.DateTimeField()
+
+    # Add image field, set it to null, blank to null
+    document_image = models.ImageField(upload_to='document_images', null=True, blank=True)
+    def __str__(self):
+        return self.docs_id
+
+    class Meta:
+        db_table = 'Document'
+
+class User(AbstractUser):
+    # Delete not use field
+    username = None
+    last_login = None
+    is_staff = None
+    is_superuser = None
+
+    password = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        db_table = 'User'
---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+from .views import UserRegisterView, UserLoginView
+
+urlpatterns = [
+    path('register', UserRegisterView.as_view(), name='register'),
+    path('login', UserLoginView.as_view(), name='login'),
+    path('documents', views.ListCreateDocumentView.as_view()),
+    path('documents/<int:pk>', views.UpdateDeleteDocumentView.as_view()),
+    path('category/<int:pk>', views.DeleteCategoryView.as_view()),
+    path('update/<int:pk>/', views.UpdateCategoryView.as_view(), name='update-category'),
+
+]
