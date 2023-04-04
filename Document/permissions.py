b22c032ebaf047c12e14efe53fe9ea2cdabf6077
Long Nguyen <long.nguyen@isods.org>
2023-04-04 15:14:59 -0400

    Commented permission.py so other API can work without authorization

--- Document/permissions.py
+++ Document/permissions.py
@@ -1,5 +1,10 @@
-from rest_framework.permissions import BasePermission
+# from rest_framework.permissions import BasePermission
+#
+# class IsAdminOrStaff(BasePermission):
+#     def has_permission(self, request, view):
+#         return request.user.role in ('admin', 'staff')
 
-class IsAdminOrStaff(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.role in ('admin', 'staff')
+
+# Implemented by Tien Truong, but requirement isn't needed anymore.
+# For future reference, if you want to check if User has permit or not
+# use isAdminOrStaff() method above.
